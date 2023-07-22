934d68a4e398e08b72868c50a1fb9535ccdd844f
CH_Hwang <hch950627@naver.com>
2023-07-23 01:41:59 +0900

    initial commit

---
+++ spring_boot_prac/src/main/java/practice/spring_boot_prac/SpringBootPracApplication.java
@@ -0,0 +1,13 @@
+package practice.spring_boot_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootPracApplication.class, args);
+  }
+
+}
