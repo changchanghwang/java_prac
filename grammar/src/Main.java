a23259c2146264f4ffd69a5efd135d7e72f97f96
CH_Hwang <hch950627@naver.com>
2023-07-22 22:38:42 +0900

    variable

---
+++ grammar/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int number = 5; // 변수 let
+    number = 4;
+    System.out.println(number); // 4
+
+    final String hello = "Hello World!";
+    System.out.println(hello);
+
+    final int constNumber = 5; // 상수 const
+    // constNumber = 4; 재할당 불가능
+    System.out.println(constNumber); // 5
+  }
+}
