514e5ffab26c62c342d59996b270a316b35af280
CH_Hwang <hch950627@naver.com>
2023-07-23 11:42:38 +0900

    getter setter

--- spring_boot_prac/src/main/java/practice/spring_boot_prac/controller/HelloController.java
+++ spring_boot_prac/src/main/java/practice/spring_boot_prac/controller/HelloController.java
@@ -1,13 +1,37 @@
 package practice.spring_boot_prac.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloController {
 
-  @RequestMapping("/hello")
-  public String hello(){
-    return "hello world";
+  @GetMapping("/hello")
+  @ResponseBody
+  public String hello(@RequestParam("name") String name) {
+    return "hello " + name;
+  }
+
+  @GetMapping("/hello-api")
+  @ResponseBody
+  public Hello HelloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    System.out.println(hello.name);
+    return hello;
+  }
+
+  static class Hello {
+    private String name;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
   }
 }
