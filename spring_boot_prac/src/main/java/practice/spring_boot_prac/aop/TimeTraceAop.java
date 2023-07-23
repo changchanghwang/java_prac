c98b4a6f59f645fa7b75577a87240334d402f518
CH_Hwang <hch950627@naver.com>
2023-07-23 23:08:38 +0900

    chore: comment

--- spring_boot_prac/src/main/java/practice/spring_boot_prac/aop/TimeTraceAop.java
+++ spring_boot_prac/src/main/java/practice/spring_boot_prac/aop/TimeTraceAop.java
@@ -5,16 +5,16 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
+@Aspect //aop로 쓰겠다는 뜻
+@Component //스프링 빈으로 등록
 public class TimeTraceAop {
 
-  @Around("execution(* practice.spring_boot_prac..*(..))")
+  @Around("execution(* practice.spring_boot_prac..*(..))") //어디서 적용될 지
   public Object excute(ProceedingJoinPoint joinPoint) throws Throwable{
     long start = System.currentTimeMillis();
     System.out.println("START: " + joinPoint.toString());
     try{
-      return joinPoint.proceed();
+      return joinPoint.proceed(); //다음 메소드로 진행
     }finally {
       long finish = System.currentTimeMillis();
       long timeMs = finish - start;
